cf42a6ca7ad993bf4a001a1ea868254bc1cd5cdc
zouchen <z304307393@163.com>
2022-01-07 18:05:54 +0800

    test: add unit test

---
+++ src/packages/use-boolean-state/__tests__/index.test.ts
@@ -0,0 +1,38 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import useBooleanState from '../index';
+
+function useTemplateState(initialState: boolean) {
+  return renderHook(() => {
+    const [state, patch] = useBooleanState(initialState);
+    return {
+      state,
+      patch,
+    }
+  })
+}
+
+describe('test useBooleanState', () => {
+
+  it('when initialState boolean', () => {
+    const hook = useTemplateState(false)
+    expect(hook.result.current.state).toEqual(false)
+  })
+
+  it('when trigger toggle', () => {
+    const hook = useTemplateState(false)
+    act(() => hook.result.current.patch.toggle())
+    expect(hook.result.current.state).toEqual(true)
+  })
+
+  it('when trigger setFalse', () => {
+    const hook = useTemplateState(false)
+    act(() => hook.result.current.patch.setFalse())
+    expect(hook.result.current.state).toEqual(false)
+  })
+
+  it('when trigger setTrue', () => {
+    const hook = useTemplateState(false)
+    act(() => hook.result.current.patch.setTrue())
+    expect(hook.result.current.state).toEqual(true)
+  })
+})
---
+++ src/packages/use-next-effect/__tests__/index.test.ts
@@ -0,0 +1,16 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useNextEffect from '../index';
+
+describe('test useNextEffect', () => {
+  it('when trigger effect', () => {
+    let mountedState = 1;
+    const hook = renderHook(() => {
+      useNextEffect(() => {
+        mountedState = 2
+      }, [])
+    })
+    expect(mountedState).toEqual(1)
+    hook.rerender()
+    expect(mountedState).toEqual(1)
+  })
+})
